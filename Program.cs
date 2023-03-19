c92e35cb6aaa9e5f80074f9bc32cf98e8485fdd0
Dungeon_Master <Dungeon_Master@DESKTOP-V567CMO>
2023-03-19 17:25:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using VA_2laba_Kalculator;
+
+namespace lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UI.mainMenu();
+        }
+    }
+}
